657e05bd051bbdeb51d9c1f348fad25782e5d5cb
luker <leonogueirac@gmail.com>
2024-06-13 19:46:06 -0300

    feat: Adding fzf and starship
    test

--- config/terminal/default.nix
+++ config/terminal/default.nix
@@ -5,10 +5,12 @@ let
 #     target = "./config/alacritty";
      config = builtins.readFile ./alacritty.toml;
      theme = builtins.readFile ./catppuccin-mocha.toml;
+     bashrc = builtins.readFile ./.bashrc;
 in
 {
      home.file.".config/alacritty/alacritty.toml".text = ''${config}'';
      home.file.".config/alacritty/catppuccin-mocha.toml".text = ''${theme}'';
+     home.file.".bashrc".text = ''${bashrc}'';
 
 #     home.activation.installSymlinks = {
 #	home.file."${target}".source = source;
