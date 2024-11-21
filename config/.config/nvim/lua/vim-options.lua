0701dbab185c48d6629991dbe65eb6c9022f6358
leonogc <leonogueirac@gmail.com>
2024-11-22 00:27:18 +0300

    chore: Adding neovim config

---
+++ config/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,11 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+
+vim.api.nvim_set_option("clipboard", "unnamed")
